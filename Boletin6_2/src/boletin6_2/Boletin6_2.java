a11e3e470b94fbf0a21560462b3d60c49c14245b
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-10-29 13:23:00 +0100

    Terminado

---
+++ Boletin6_2/src/boletin6_2/Boletin6_2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_2;
+
+/**
+ *
+ * @author Lara Vazquez Dorna
+ */
+public class Boletin6_2 {
+
+    /**
+     * 2- Implementa un programa no que se tecleen dous números de tipo short .
+     * Se o primeiro é maior ou igual que o segundo,visualizaremos a resta . En
+     * calquera caso visualizaremos a suma .
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cuentas obx1= new Cuentas();
+        obx1.operarNum();
+    }
+
+}
---
+++ Boletin6_2/src/boletin6_2/Cuentas.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_2;
+
+import javax.swing.JOptionPane;
+
+/**
+ * 2- Implementa un programa no que se tecleen dous números de tipo short . Se o
+ * primeiro é maior ou igual que o segundo,visualizaremos a resta . En calquera
+ * caso visualizaremos a suma .
+ *
+ *
+ * @author lvazquezdorna
+ */
+public class Cuentas {
+
+    public void operarNum() {
+        int num1,num2;
+
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("teclea el primer operador: "));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("teclea el segundo operador: "));
+          JOptionPane.showMessageDialog(null, ("SUMA: "+num1 +" + "+num2 +" = " +(num1+num2)));
+        if (num1 >=num2) {
+            JOptionPane.showMessageDialog(null, ("RESTA: "+num1 +" - "+num2 +" = " + (num1-num2)));
+        }
+
+    }
+    
+    
+}
