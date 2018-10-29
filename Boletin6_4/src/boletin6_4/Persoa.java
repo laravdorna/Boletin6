e1932223558aae0eb84018487086adfc703ef714
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-10-29 13:23:49 +0100

    Iniciado

---
+++ Boletin6_4/src/boletin6_4/Persoa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_4;
+
+/**
+ * 4-Coñecidos o nome e o peso de dúas persoas, queremos escribir os datos da
+ * que pesa máis e, a diferenza de peso entre elas .
+ *
+ * @author lvazquezdorna
+ */
+public class Persoa {
+private String nombe;
+private double peso;
+
+
+}
