111fea51c2d8d748986a68dee350d4d0ab3ad586
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-11-05 11:32:19 +0100

    con JOptionPanel. if encadenados

---
+++ Boletin6_7/src/boletin6_7/Boletin6_7.java
@@ -0,0 +1,26 @@
+/*
+ *- Codifica un programa que, utilizando un menú de opcións, calcule a superficie de distintas figuras .
+O usuario premerá a opción desexada .Según esta o programa pediralle os datos precisos e, utilizando un switch…case visualizará o resultado .
+No caso de premer unha opción que non teña o menú visualizar unha mensaxe de “ opción incorrecta “.
+1…. Cadrado
+2…. Triangulo
+3…. Circulo
+
+ */
+
+package boletin6_7;
+
+/**
+ *
+ * @author Lara Vazquez Dorna
+ */
+public class Boletin6_7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
